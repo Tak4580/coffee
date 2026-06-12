806c6aaec2d8b9d89806ad3fc1536d9585272bcf
Tak4580 <tak4580@gmail.com>
2026-06-12 20:49:30 +0900

    通知Workerの設定例を更新

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   liffId: '1234567890-AbcdEfgh',
-  apiBaseUrl: 'https://example.com/api',
+  apiBaseUrl: 'https://takahiro-coffee-order.example.workers.dev',
   lineOfficialAccountUrl: 'https://lin.ee/xxxxxxxx'
 };

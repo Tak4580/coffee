cf0524d09e3b9919d57db5c41858c43c1ea02a19
Tak4580 <tak4580@gmail.com>
2026-06-13 11:04:05 +0900

    新しいLIFF IDとGAS通知URLを設定

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   // LINE Developers Consoleで発行されたLIFF IDを設定します。
-  liffId: '2010316929-viV90cPK',
+  liffId: '2010384179-FDCKBM32',
   // 注文通知用バックエンドのURL。設定すると公式アカウントから確認通知を送ります。
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxo73nW-2xtQUq1MKX7CNYMUmMrjj-E7WGMKiF75_ustZxW0shoWtRSKkYnEQKkeOG53Q/exec',
   lineOfficialAccountUrl: ''
 };

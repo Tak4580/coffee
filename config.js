3f5fe4c13a0222a158a974e7ffbb90a9ee5a8b09
Tak4580 <tak4580@gmail.com>
2026-06-13 11:35:38 +0900

    GASの新しいデプロイURLへ切り替え

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   // LINE Developers Consoleで発行されたLIFF IDを設定します。
   liffId: '2010384179-FDCKBM32',
   // 注文通知用バックエンドのURL。設定すると公式アカウントから確認通知を送ります。
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxo73nW-2xtQUq1MKX7CNYMUmMrjj-E7WGMKiF75_ustZxW0shoWtRSKkYnEQKkeOG53Q/exec',
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyFvW1uGJ1yandVzixQGmBAVpSEBXVCuhTnvl6wASj_iR-QUXusfmSgSa_Ja4dRC_HZGg/exec',
   lineOfficialAccountUrl: ''
 };
 
